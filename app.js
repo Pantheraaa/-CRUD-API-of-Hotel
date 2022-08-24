2a95acf5bcacd2ade5d4916c35e8a381d341b433
Rahul Kumar <95248963+Pantheraaa@users.noreply.github.com>
2022-08-24 16:44:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,13 +6,3 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json())
 app.use('/hotel', hotelRoute);
-
-// app.post('/', (req, res) => {
-//     console.log('Post called');
-//     let hotel = req.body;
-//     console.log(hotel);
-//     res.json({
-//         message: "List of hotels",
-//         user: hotel,
-//     })
-// })
